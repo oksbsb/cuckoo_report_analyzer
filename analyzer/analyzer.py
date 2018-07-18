26203a0ec422c83b79118d5889853567b6afccee
Albert <albertkelvinhutapea@gmail.com>
2018-07-18 11:53:21 +0700

    analyzer fix

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -62,113 +62,62 @@ def virus_total_report(md5):
 	        print "Antivirus' update:", antivirus[2]
 	        print "Malware:", malware
 
+def count_score(matches):
+	temp = []
+	count = 0
 
-# def comparing(first_seq,second_seq,malware_seq):
-# 	score = 0
-# 	position = 0
-# 	array = []
-# 	count = 0
-# 	for data in first_seq:
-# 		if count == 1000:
-# 			break
-# 		for dat in data:
-# 			for da in second_seq:	
-# 				if (da==dat):
-# 					score+=1
-# 				if da in malware_seq:
-# 					print malware_seq
-# 		array.append((data[0],score))
-# 		score = 0
-# 		count += 1
+	for match in matches:
+		if (int(match[1])>=7):
+			count+=1
+			print "Menemukan "+str(count)+" malicious behavior"	
+			temp.append(match)	
+	if (temp):
+		print "\n=======FILE ADALAH MALWARE=======\n"	
+		print "Karena melakukan hal berikut :"
+		for match in matches:
+			if (int(match[1])>=7):
+				print "Behavior :	" + match[0]
+				print "Score 	:	" + match[1]
+				print "Functions:" 
+				for func in match[2]:
+					print "	"+func
+	else :
+		print "\n=======FILE TIDAK MALWARE=======\n"	
 
+def delete_duplicate(array):
+	output = []
+	for val in array:
+		if not val in output:
+			output.append(val)
+	return output		
+						
+def analyze(malware_seq,application_api_seq):
 
-# 	min_val = 0
-# 	max_scored_element = None
-
-# 	count = 0
-# 	for data in array:
-# 		print data
-# 		if data[1] > min_val:
-# 			#menandai posisi maximum score
-# 			position = count
-			
-# 			max_scored_element = data
-# 			min_val = data[1] 
-# 		count+=1
-
-# 	print max_scored_element
-
-# 	#cek kesamaan API calls
-# 	check_API(first_seq[position],second_seq)
-
-# def check_API(first_seq,second_seq):
-# 	count = 0
-
-# 	for data in first_seq:
-# 		print data
-# 		if data in second_seq:
-# 			count += 1
-# 			print (data,count)
-
-# def read_memory_dump():
-# 	data = []
-# 	with open('../source_files/memory.dmp', 'rb') as infile:
-# 		while True:
-# 			data.append(infile.read(204800))
-# 			print hexdump.hexdump(data)
-# 			if not data:
-# 				break
-
-
-
-def detect(application_api_seq,malware_seq):
-
-	finding = []
-	apis = []
-
+	no_dup = delete_duplicate(application_api_seq)
+	
+	match_api = []
+	matches = []
+	temp = []
+	
 	for malware_csv_rows in malware_seq:
-		
-		for api in application_api_seq:
-			if api in malware_csv_rows:	
-				apis.append(api)
-				print api 
-				print malware_csv_rows[0]
-
-			if apis:
-				finding.append([malware_csv_rows[0],malware_csv_rows[1],apis])
-
-
-				
-
-	if finding:
-		print finding
-		for summary in finding:
-
-			if (summary[1] > 7):
-				print "Executable is categorized as "+summary[0]
-				print "and scored "+summary[1]
-				print "APIs called("+summary[0]+") : "
-				for api in summary[2:]:
-					print api
-			if (summary[1]==7):
-				print "File is similar to a malware "+summary[0]
-				print "APIs called("+summary[0]+") : "
-				for api in summary[2:]:
-					print api 			
-			if (summary[1]<=5):
-				print "File is similar to a malware "+summary[0]
-				print "APIs called("+summary[0]+") : "
-				for api in summary[2:]:
-					print api
-	else:
-		print "File is Not a malware"	 			
-
+		# print malware_csv_rows
+		for api in no_dup:
+			if api in malware_csv_rows:
+				match_api.append(api)
+		if match_api:
+			print match_api
+			temp = match_api
+			matches.append([malware_csv_rows[0],malware_csv_rows[1],temp])
+		match_api = []
+	count_score(matches)
 
 
 def main():
 	processes = load_process_data()
 
 	application_api_seq = []
+	malware_seq = data_reader.read_csv_malware_API()
+
 	calls = processes['calls']
 
 	for call in calls:
@@ -195,9 +144,7 @@ def main():
 		md5 = infos['md5']
 
 
-	first_seq = data_reader.read_csv()
-	malware_seq = data_reader.read_csv_malware_API()
-	detect(application_api_seq,malware_seq)
+	analyze(malware_seq,application_api_seq)
 
 	# virus_total_report(md5)
 
